b4a7e547acd18f60418c9f288cad0b1818dda4e9
WebDevChaos987 <avery.mancini.2016@gmail.com>
2026-07-16 17:37:07 +0800

    Setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="actl",
-    version="0.001",          # Bump the version to 0.001 to force a rewrite!
+    version="0.00000",          # Bump the version to (version) to force a rewrite!
     packages=["actl"],
 )
