4605949be2a9e8c15aa2daa3e1ff2b097dedd139
WebDevChaos987 <avery.mancini.2016@gmail.com>
2026-07-16 16:40:04 +0800

    Refactor wait function with docstring and parameter rename
    Updated wait function to include a docstring and changed parameter name from 'intensity' to 'nt'.

--- actl/wait.py
+++ actl/wait.py
@@ -1,3 +1,6 @@
-def wait(intensity):
-	for _ in range(intensity):
-		count += 1
+def wait(nt):
+    """
+    Pauses execution using a basic math calculation loop.
+    """
+    for i in range(nt):
+        pass
